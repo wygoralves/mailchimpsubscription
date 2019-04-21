12bd3af9b728e597869ca3b48a9164665c7ff50a
Wygor Alves <wygor96@gmail.com>
2019-04-21 01:10:31 -0300

    Finalizou a parte da integração

--- main.js
+++ main.js
@@ -14,15 +14,32 @@ app.post("/", function(req, res){
     const fname = req.body.firstname;
     const lname = req.body.lastname;
     const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data);
 
     const options = {
         url: "https://us20.api.mailchimp.com/3.0/lists/2ac565a4d3",
         method: "POST",
-
+        headers: {
+            "Authorization": "wygor96 8ba28efaf6f2529fbc51ec5c60052bcc-us20"
+        },
+        body: jsonData
     };
+
     request(options, function(error, response, body){
-        if (error) {
-            console.log(error);
+        if (response.statusCode(200)) {
+            console.log("Success!");
         }else{
             console.log(response.statusCode);
         }
@@ -33,6 +50,3 @@ app.post("/", function(req, res){
 app.listen(3000, function(){
     console.log("Node server running on port 3000!");
 });
-
-// mailchimp api key: 8ba28efaf6f2529fbc51ec5c60052bcc-us20
-// list id 49709
