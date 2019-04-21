89380e280fe3c864ca6d7b17f0554cd5da0582d8
Wygor Alves <wygor96@gmail.com>
2019-04-21 00:49:08 -0300

    Ajeitei o post request de envio dos emails

--- main.js
+++ main.js
@@ -15,10 +15,24 @@ app.post("/", function(req, res){
     const lname = req.body.lastname;
     const email = req.body.email;
 
-    console.log("Here are your credencials: "+fname+ " " +lname+" "+email+".");
+    const options = {
+        url: "https://us20.api.mailchimp.com/3.0/lists/2ac565a4d3",
+        method: "POST",
+
+    };
+    request(options, function(error, response, body){
+        if (error) {
+            console.log(error);
+        }else{
+            console.log(response.statusCode);
+        }
+    });
 });
 
 
 app.listen(3000, function(){
     console.log("Node server running on port 3000!");
 });
+
+// mailchimp api key: 8ba28efaf6f2529fbc51ec5c60052bcc-us20
+// list id 49709
