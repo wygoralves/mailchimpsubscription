b0f0e0499dd2bd18e0386eb6333e1ea692f39f1b
Wygor Alves <wygor96@gmail.com>
2019-04-21 00:13:49 -0300

    Alterei o jeito de subir os assets e puxei as vars do form pro server

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/index.html");
 });
 
-app.get("/style.css", function(req,res){
-    res.sendFile(__dirname+"/style.css");
+app.post("/", function(req, res){
+    const fname = req.body.firstname;
+    const lname = req.body.lastname;
+    const email = req.body.email;
+
+    console.log("Here are your credencials: "+fname+ " " +lname+" "+email+".");
 });
 
-app.get("/logo.png", function(req,res){
-    res.sendFile(__dirname+"/logo.png");
-});
 
 app.listen(3000, function(){
     console.log("Node server running on port 3000!");
