80ddf5e8892345433a9263ee995f8476f64090e7
Wygor Alves <wygor96@gmail.com>
2019-04-21 15:24:59 -0300

    make it better

--- main.js
+++ main.js
@@ -54,6 +54,6 @@ app.post("/", function(req, res){
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Node server running on port 3000!");
 });
